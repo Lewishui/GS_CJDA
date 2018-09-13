b1f8212ffbe8f58586dfd713318eb3bd4da02f24
Lewishui <lewis.hui@qq.com>
2018-09-13 17:06:04 +0800

    up

--- GS_CJDA/frmLogin.cs
+++ GS_CJDA/frmLogin.cs
@@ -12,9 +12,49 @@ namespace GS_CJDA
 {
     public partial class frmLogin : Form
     {
+        public log4net.ILog ProcessLogger;
+        public log4net.ILog ExceptionLogger;
+        private TextBox txtSAPPassword;
+        private CheckBox chkSaveInfo;
+        Sunisoft.IrisSkin.SkinEngine se = null;
+        frmAboutBox aboutbox;
+        private frmMain frmMain;
+        //存放要显示的信息
+        List<string> messages;
+        //要显示信息的下标索引
+        int index = 0;
         public frmLogin()
         {
             InitializeComponent();
+            aboutbox = new frmAboutBox();
         }
+
+        private void btmain_Click(object sender, EventArgs e)
+        {
+
+            if (frmMain == null)
+            {
+                frmMain = new frmMain();
+                frmMain.FormClosed += new FormClosedEventHandler(FrmOMS_FormClosed);
+            }
+            if (frmMain == null)
+            {
+                frmMain = new frmMain();
+            }
+            frmMain.Show(this.dockPanel2);
+        }
+        void FrmOMS_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (sender is frmMain)
+            {
+                frmMain = null;
+            }
+        }
+
+        private void 关于系统ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            aboutbox.ShowDialog();
+        } 
+
     }
 }
